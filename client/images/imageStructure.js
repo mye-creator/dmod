c7ef5218ecf8e0152650891696a9d79576a65199
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2020-12-24 15:03:39 -0700

    Update imageStructure.js

--- client/images/imageStructure.js
+++ client/images/imageStructure.js
@@ -1,3 +1,3 @@
-exports.dmodv2 = './dmodv2.png'
-exports.dmod_transparent = './transparent_dmod.png'
+exports.dmodv2 = 'https://cdn.discordapp.com/attachments/756076154217168937/791768591514468372/dmodv2.png'
+exports.dmod_transparent = 'https://cdn.discordapp.com/attachments/756076154217168937/791768610674442260/transparent_dmod.png'
 exports.mainColor = '#181944'
